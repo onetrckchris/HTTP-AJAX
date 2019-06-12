b54af4b0eff2527456e4c8a271cdce60da2e82ab
onetrckchris <onetrckchris@gmail.com>
2019-06-12 12:12:45 -0700

    added basic friends and friend components

---
+++ friends/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Friends from './components/Friends';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Friends />
+    </div>
+  );
+}
+
+export default App;
---
+++ friends/src/components/Friends.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Friend from './Friend';
+
+const Friends = props => {
+    return (
+        <div>
+            <Friend />
+        </div>
+    )
+}
+
+export default Friends;
